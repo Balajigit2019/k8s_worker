2ab0f5cd243682a1917789ea68352e291e90c650
Balajigit2019 <47142716+Balajigit2019@users.noreply.github.com>
2022-05-05 18:05:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -198,23 +198,29 @@ connection {
     host = self.public_ip
 	
 }
+/*	
 provisioner "remote-exec" {
     inline = [
 	 "sudo apt update -y",
          "sudo apt install ansible -y"
 ]
   }
+*/	
 provisioner "file" {
     source = "kubernetes_join_command"
     destination = "/tmp/kubernetes_join_command"
   }
+/*	
 provisioner "file" {
     source = "k8s.yaml"
     destination = "/tmp/k8s.yaml"
   }
+}
+/*
 provisioner "remote-exec" {
    inline = [
     "sudo ansible-playbook /tmp/k8s.yaml"
 ]
   }
 }
+*/
